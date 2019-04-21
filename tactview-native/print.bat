5ab98b01abbdd72a754ef03d9b0216cedfa862db
helospark <helospark@gmail.com>
2019-04-21 21:26:35 +0200

    POC remove git accidents

--- tactview-native/print.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal ENABLEDELAYEDEXPANSION
-
-for /r %%i in (*.cpp) do (
-	set inputFile="%%~ni.cpp"
-	set objFile="%%~ni.obj"
-	set dllFile="%%~ni.dll"
-	echo "!inputFile! !objFile! !dllFile!" 
-)
