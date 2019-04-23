4c4a4d857243419a8e45e4d580474247f7e8a2a4
helospark <helospark@gmail.com>
2019-04-23 20:27:59 +0200

    POC update windows build

--- create-release-win-x64.sh
+++ create-release-win-x64.sh
@@ -17,9 +17,9 @@ cp tactview-native/startup.exe release/win64/tactview.exe
 
 echo "Copying dynamic library dependencies"
 
-BLACKLISTED_DEPENDENCIES="System32\|windows"
+BLACKLISTED_DEPENDENCIES="advapi32.dll\|bcrypt.dll\|bcryptPrimitives.dll\|cfgmgr32.dll\|combase.dll\|COMDLG32.dll\|CONCRT140.dll\|CRYPT32.dll\|CRYPTBASE.DLL\|cryptsp.dll\|d3d11.dll\|dxgi.dll\|GDI32.dll\|gdi32full.dll\|IMM32.DLL\|kernel.appcore.dll\|KERNEL32.DLL\|KERNELBASE.dll\|ksuser.dll\|MF.dll\|MFCORE.DLL\|MFPlat.DLL\|MFReadWrite.dll\|MSASN1.dll\|msvcp_win.dll\|MSVCP140.dll\|msvcrt.dll\|ntdll.dll\|ole32.dll\|OLEAUT32.dll\|powrprof.dll\|profapi.dll\|RPCRT4.dll\|RTWorkQ.DLL\|sechost.dll\|Secur32.dll\|shcore.dll\|SHELL32.dll\|shlwapi.dll\|SHLWAPI.dll\|SSPICLI.DLL\|ucrtbase.dll\|USER32.dll\|win32u.dll\|windows.storage.dll\|WS2_32.dll\|COMCTL32.dll"
 
-ldd tactview-native/*.dll | grep "=> /" | grep -i -v $BLACKLISTED_DEPENDENCIES | awk '{print $3}' | sort -u | xargs -I '{}' cp -v '{}' release/win64.
+ldd tactview-native/*.dll | grep "=> /" | grep -i -v $BLACKLISTED_DEPENDENCIES | awk '{print $3}' | sort -u | xargs -I '{}' cp -v '{}' release/win64/.
 
 cd release
 
