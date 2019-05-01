c9210a0a1f9d63fae71b4886ffb18a658d961317
helospark <helospark@gmail.com>
2019-05-01 21:44:49 +0200

    POC make it also work on Windows

--- tactview-native/startup_win.cpp
+++ tactview-native/startup_win.cpp
@@ -1,10 +1,12 @@
 #include <cstdlib>
+#include <iostream>
 
 int main() {
   int statusCode = 0;
   do {
-    system("\"java-runtime\\bin\\java\" -Xmx8g -jar tactview.jar");
+    statusCode = system("\"java-runtime\\bin\\java\" -Xmx8g -jar tactview.jar");
     std::cout << "Tactview returned " << statusCode << std::endl;
   } while (statusCode == 3);
   std::cout << "Exiting tactview, bye!" << std::endl;
 }
+
