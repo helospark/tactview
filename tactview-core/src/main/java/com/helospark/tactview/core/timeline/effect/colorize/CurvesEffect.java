0cfabc000baf4c69b7a3e5f34c7012dea0610b34
helospark <helospark@gmail.com>
2020-09-24 11:39:51 +0200

    Different curves

--- tactview-core/src/main/java/com/helospark/tactview/core/timeline/effect/colorize/CurvesEffect.java
+++ tactview-core/src/main/java/com/helospark/tactview/core/timeline/effect/colorize/CurvesEffect.java
@@ -2,22 +2,22 @@ package com.helospark.tactview.core.timeline.effect.colorize;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.helospark.tactview.core.clone.CloneRequestMetadata;
+import com.helospark.tactview.core.decoder.framecache.GlobalMemoryManagerAccessor;
 import com.helospark.tactview.core.save.LoadMetadata;
 import com.helospark.tactview.core.timeline.StatelessEffect;
 import com.helospark.tactview.core.timeline.StatelessVideoEffect;
 import com.helospark.tactview.core.timeline.TimelineInterval;
 import com.helospark.tactview.core.timeline.effect.StatelessEffectRequest;
 import com.helospark.tactview.core.timeline.effect.interpolation.ValueProviderDescriptor;
-import com.helospark.tactview.core.timeline.effect.interpolation.interpolator.StepStringInterpolator;
 import com.helospark.tactview.core.timeline.effect.interpolation.pojo.Color;
 import com.helospark.tactview.core.timeline.effect.interpolation.provider.CurveProvider;
 import com.helospark.tactview.core.timeline.effect.interpolation.provider.CurveProvider.KnotAwareUnivariateFunction;
 import com.helospark.tactview.core.timeline.effect.interpolation.provider.PointProvider;
-import com.helospark.tactview.core.timeline.effect.interpolation.provider.ValueListElement;
-import com.helospark.tactview.core.timeline.effect.interpolation.provider.ValueListProvider;
+import com.helospark.tactview.core.timeline.image.ClipImage;
 import com.helospark.tactview.core.timeline.image.ReadOnlyClipImage;
 import com.helospark.tactview.core.util.IndependentPixelOperation;
 import com.helospark.tactview.core.util.MathUtil;
@@ -25,9 +25,7 @@ import com.helospark.tactview.core.util.ReflectionUtil;
 
 public class CurvesEffect extends StatelessVideoEffect {
     private IndependentPixelOperation independentPixelOperation;
-
-    private CurveProvider curveProvider;
-    private ValueListProvider<CurvesTargetValueListElement> targetProvider;
+    private Map<String, CurveProviderEntry> valueProviders;
 
     public CurvesEffect(TimelineInterval interval, IndependentPixelOperation independentPixelOperation) {
         super(interval);
@@ -48,77 +46,113 @@ public class CurvesEffect extends StatelessVideoEffect {
     public ReadOnlyClipImage createFrame(StatelessEffectRequest request) {
         int[] valueLut = new int[256];
 
-        KnotAwareUnivariateFunction curve = curveProvider.getValueAt(request.getEffectPosition());
+        ReadOnlyClipImage image = ClipImage.copyOf(request.getCurrentFrame());
 
-        double value = 0.0;
-        for (int i = 0; i < 256; ++i) {
-            valueLut[i] = MathUtil.clamp((int) (curve.value(value) * 255.0), 0, 255);
-            value += (1.0 / 255);
+        for (CurveProviderEntry curveProvider : valueProviders.values()) {
+            CurveProvider provider = curveProvider.provider;
+
+            KnotAwareUnivariateFunction curve = provider.getValueAt(request.getEffectPosition());
+
+            double value = 0.0;
+            for (int i = 0; i < 256; ++i) {
+                valueLut[i] = MathUtil.clamp((int) (curve.value(value) * 255.0), 0, 255);
+                value += (1.0 / 255);
+            }
+
+            CurvesTargetFunction mappingFunction = curveProvider.function;
+
+            ReadOnlyClipImage newImage = independentPixelOperation.createNewImageWithAppliedTransformation(image, pixelRequest -> {
+                mappingFunction.createResult(pixelRequest.input, pixelRequest.output, valueLut);
+            });
+
+            GlobalMemoryManagerAccessor.memoryManager.returnBuffer(image.getBuffer());
+            image = newImage;
         }
-
-        CurvesTargetFunction mappingFunction = targetProvider.getValueAt(request.getEffectPosition()).function;
-
-        return independentPixelOperation.createNewImageWithAppliedTransformation(request.getCurrentFrame(), pixelRequest -> {
-            mappingFunction.createResult(pixelRequest.input, pixelRequest.output, valueLut);
-        });
+        return image;
     }
 
     @Override
     protected void initializeValueProviderInternal() {
-        List<PointProvider> points = new ArrayList<>();
-        points.add(PointProvider.of(0.0, 0.0));
-        points.add(PointProvider.of(0.3, 0.2));
-        points.add(PointProvider.of(0.7, 0.8));
-        points.add(PointProvider.of(1.0, 1.0));
-
-        curveProvider = new CurveProvider(0.0, 1.0, 0.0, 1.0, points);
-
-        targetProvider = new ValueListProvider<>(createValueProviders(), new StepStringInterpolator("rgb"));
+        valueProviders = createValueProviders();
     }
 
-    private List<CurvesTargetValueListElement> createValueProviders() {
-        return List.of(
-                new CurvesTargetValueListElement("rgb", (input, output, lut) -> {
-                    output[0] = lut[input[0]];
-                    output[1] = lut[input[1]];
-                    output[2] = lut[input[2]];
-                    output[3] = input[3];
-                }),
-                new CurvesTargetValueListElement("red", (input, output, lut) -> {
-                    output[0] = lut[input[0]];
-                    output[1] = input[1];
-                    output[2] = input[2];
-                    output[3] = input[3];
-                }),
-                new CurvesTargetValueListElement("green", (input, output, lut) -> {
-                    output[0] = input[0];
-                    output[1] = lut[input[1]];
-                    output[2] = input[2];
-                    output[3] = input[3];
-                }),
-                new CurvesTargetValueListElement("blue", (input, output, lut) -> {
-                    output[0] = input[0];
-                    output[1] = input[1];
-                    output[2] = lut[input[2]];
-                    output[3] = input[3];
-                }),
-                new CurvesTargetValueListElement("hue", (input, output, lut) -> {
-                    Color hsl = createHslFromInput(input);
-                    hsl.red = lut[(int) (hsl.red * 255.0)] / 255.0;
-                    copyHslToResult(input, output, hsl);
-                }),
-                new CurvesTargetValueListElement("saturation", (input, output, lut) -> {
-                    Color hsl = createHslFromInput(input);
-                    hsl.green = lut[(int) (hsl.green * 255.0)] / 255.0;
-                    copyHslToResult(input, output, hsl);
-                }),
-                new CurvesTargetValueListElement("lightness", (input, output, lut) -> {
-                    Color hsl = createHslFromInput(input);
-                    hsl.blue = lut[(int) (hsl.blue * 255.0)] / 255.0;
-                    copyHslToResult(input, output, hsl);
-                })
+    private Map<String, CurveProviderEntry> createValueProviders() {
+        return Map.of("rgb",
+                new CurveProviderEntry(
+                        createCurveProvider(),
+                        (input, output, lut) -> {
+                            output[0] = lut[input[0]];
+                            output[1] = lut[input[1]];
+                            output[2] = lut[input[2]];
+                            output[3] = input[3];
+                        }),
 
-        );
+                "red",
+                new CurveProviderEntry(
+                        createCurveProvider(),
+                        (input, output, lut) -> {
+                            output[0] = lut[input[0]];
+                            output[1] = input[1];
+                            output[2] = input[2];
+                            output[3] = input[3];
+                        }),
+
+                "green",
+                new CurveProviderEntry(
+                        createCurveProvider(),
+                        (input, output, lut) -> {
+                            output[0] = input[0];
+                            output[1] = lut[input[1]];
+                            output[2] = input[2];
+                            output[3] = input[3];
+                        }),
+
+                "blue",
+                new CurveProviderEntry(
+                        createCurveProvider(),
+                        (input, output, lut) -> {
+                            output[0] = input[0];
+                            output[1] = input[1];
+                            output[2] = lut[input[2]];
+                            output[3] = input[3];
+                        }),
+
+                "hue",
+                new CurveProviderEntry(
+                        createCurveProvider(),
+                        (input, output, lut) -> {
+                            Color hsl = createHslFromInput(input);
+                            hsl.red = lut[(int) (hsl.red * 255.0)] / 255.0;
+                            copyHslToResult(input, output, hsl);
+                        }),
+
+                "saturation",
+                new CurveProviderEntry(
+                        createCurveProvider(),
+                        (input, output, lut) -> {
+                            Color hsl = createHslFromInput(input);
+                            hsl.green = lut[(int) (hsl.green * 255.0)] / 255.0;
+                            copyHslToResult(input, output, hsl);
+                        }),
+
+                "lightness",
+                new CurveProviderEntry(
+                        createCurveProvider(),
+                        (input, output, lut) -> {
+                            Color hsl = createHslFromInput(input);
+                            hsl.blue = lut[(int) (hsl.blue * 255.0)] / 255.0;
+                            copyHslToResult(input, output, hsl);
+                        }));
+    }
+
+    private CurveProvider createCurveProvider() {
+        List<PointProvider> points = new ArrayList<>();
+        points.add(PointProvider.of(0.0, 0.0));
+        points.add(PointProvider.of(0.3, 0.3));
+        points.add(PointProvider.of(0.7, 0.7));
+        points.add(PointProvider.of(1.0, 1.0));
+
+        return new CurveProvider(0.0, 1.0, 0.0, 1.0, points);
     }
 
     private Color createHslFromInput(int[] input) {
@@ -138,16 +172,17 @@ public class CurvesEffect extends StatelessVideoEffect {
 
     @Override
     protected List<ValueProviderDescriptor> getValueProvidersInternal() {
-        ValueProviderDescriptor targetDescriptor = ValueProviderDescriptor.builder()
-                .withKeyframeableEffect(targetProvider)
-                .withName("target")
-                .build();
-        ValueProviderDescriptor valueCurveDescriptor = ValueProviderDescriptor.builder()
-                .withKeyframeableEffect(curveProvider)
-                .withName("value")
-                .build();
+        List<ValueProviderDescriptor> result = new ArrayList<>();
+        for (var entry : valueProviders.entrySet()) {
 
-        return List.of(targetDescriptor, valueCurveDescriptor);
+            ValueProviderDescriptor targetDescriptor = ValueProviderDescriptor.builder()
+                    .withKeyframeableEffect(entry.getValue().provider)
+                    .withName(entry.getKey())
+                    .build();
+            result.add(targetDescriptor);
+        }
+
+        return result;
     }
 
     @Override
@@ -155,18 +190,19 @@ public class CurvesEffect extends StatelessVideoEffect {
         return new CurvesEffect(this, cloneRequestMetadata);
     }
 
-    static class CurvesTargetValueListElement extends ValueListElement {
+    static interface CurvesTargetFunction {
+        void createResult(int[] input, int[] output, int[] lut);
+    }
+
+    static class CurveProviderEntry {
+        CurveProvider provider;
         CurvesTargetFunction function;
 
-        public CurvesTargetValueListElement(String id, CurvesTargetFunction function) {
-            super(id, id);
+        public CurveProviderEntry(CurveProvider provider, CurvesTargetFunction function) {
+            this.provider = provider;
             this.function = function;
         }
 
-    }
-
-    static interface CurvesTargetFunction {
-        void createResult(int[] input, int[] output, int[] lut);
     }
 
 }
